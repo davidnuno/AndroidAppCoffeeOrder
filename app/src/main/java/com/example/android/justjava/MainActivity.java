5ff80ad09a0467890f77864e284bc46fba8f83ed
David Nuno <davidnuno281@gmail.com>
2016-11-09 12:11:47 -0600

    Added a message to total.

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -6,6 +6,7 @@
  */
 
 package com.example.android.justjava;
+
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -29,19 +30,21 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        displayPrice(5* quantity);
+        String priceMessage = "Total: ";
+
+        displayMessage(priceMessage);
+        //displayPrice(5 * quantity);
     }
 
     /**
-     * This method is called when the increment button is clicked.
+     * This method is called when the plus button is clicked.
      */
     public void increment(View view) {
         quantity++;
         display(quantity);
-        //displayPrice(5* quantity);
     }
     /**
-     * This method is called when the increment button is clicked.
+     * This method is called when the negative button is clicked.
      */
     public void decrement(View view) {
         if(quantity == 0) {
@@ -49,7 +52,6 @@ public class MainActivity extends AppCompatActivity {
         }
         quantity--;
         display(quantity);
-        //displayPrice(5* quantity);
     }
 
     /**
@@ -58,6 +60,13 @@ public class MainActivity extends AppCompatActivity {
     private void display(int number) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + number);
+    }
+    /**
+     * This method displays the given text on the screen.
+     */
+    private void displayMessage(String message) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(message);
     }
     /**
      * This method displays the given price on the screen.
