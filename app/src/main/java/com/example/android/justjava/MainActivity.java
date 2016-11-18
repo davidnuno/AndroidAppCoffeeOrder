48809304946fd0d8d8c1f0e84bad9950b14642ea
David Nuno <davidnuno281@gmail.com>
2016-11-18 16:32:44 -0600

    Added the name text field.

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -7,14 +7,20 @@
 
 package com.example.android.justjava;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.text.NumberFormat;
+
+import static android.R.attr.name;
 
 /**
  * This app displays an order form to order coffee.
@@ -22,10 +28,7 @@ import java.text.NumberFormat;
 public class MainActivity extends AppCompatActivity {
     /**
      * The number of coffee cups. */
-    int quantity = 0;
-    /**
-     * The price per coffee cup. */
-    int price = 5;
+    int quantity = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,40 +39,85 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        CheckBox whippedCreamCheckbox = (CheckBox) findViewById(R.id.whipped_cream_checkbox);
+        // Figure out if the user wants whipped cream topping
+        CheckBox whippedCreamCheckBox = (CheckBox) findViewById(R.id.whipped_cream_checkbox);
+        boolean hasWhippedCream = whippedCreamCheckBox.isChecked();
 
-        boolean hasWhippedCream = whippedCreamCheckbox.isChecked();
+        // Figure out if the user wants whipped cream topping
+        CheckBox chocolateCheckBox = (CheckBox) findViewById(R.id.chocolate_checkbox);
+        boolean hasChocolate = chocolateCheckBox.isChecked();
 
-        String priceMessage = createOrderSummary(price, hasWhippedCream);
+        EditText nameField = (EditText) findViewById(R.id.name_field);
 
-        displayMessage(priceMessage);
-        calculatePrice();
+        String name = nameField.getText().toString();
+
+        // Calculate the price
+        int price = calculatePrice(hasWhippedCream, hasChocolate);
+
+        // Display the order summary on the screen
+        String message = createOrderSummary(name, price, hasWhippedCream, hasChocolate);
+
+        Intent orderEmail = new Intent(Intent.ACTION_SENDTO);
+        orderEmail.setData(Uri.parse("mailto:")); // only email apps should handle this
+
+        orderEmail.putExtra(Intent.EXTRA_SUBJECT, "Coffee order for " + name);
+        orderEmail.putExtra(Intent.EXTRA_TEXT, message);
+        if (orderEmail.resolveActivity(getPackageManager()) != null) {
+            startActivity(orderEmail);
+        }
+        //displayMessage(message);
     }
     /**
      * Calculates the price of the order.
      *
-     * @return the total price.
+     * @return total price
      */
-    private int calculatePrice() {
+    private int calculatePrice(boolean addWhippedCream, boolean addChocolate) {
+        //Base price for one cup of coffee.
+        int basePrice = 5;
 
-        return quantity * price;
+        //Add $1 if customer wants whipped cream.
+        if(addWhippedCream){
+            basePrice = basePrice + 1;
+        }
+
+        //Add $2 if customer wants chocolate.
+        if(addChocolate) {
+            basePrice = basePrice + 2;
+        }
+
+        //Calculate the total price.
+        return quantity * basePrice;
     }
     /**
-     * This method displays the summary of the order.
-     * */
-    private String createOrderSummary(int price, boolean addWhippedCream) {
-
-        String priceMessage = "Name: David Nuno\n";
-
-        priceMessage += "add whipped cream?: " + addWhippedCream;
-        priceMessage += "Quantity: " + quantity + "\nTotal: $" + price + "\nThank you!";
-
+     * Create summary of the order.
+     *
+     * @param theName the name.
+     * @param price of the order.
+     * @param addWhippedCream is whether or not to add whipped cream to the coffee.
+     * @param addChocolate    is whether or not to add chocolate to the coffee.
+     * @return text summary.
+     */
+    private String createOrderSummary(String theName, int price, boolean addWhippedCream, boolean addChocolate) {
+        String priceMessage = "Name: " + theName;
+        priceMessage += "\nAdd whipped cream? " + addWhippedCream;
+        priceMessage += "\nAdd chocolate? " + addChocolate;
+        priceMessage += "\nQuantity: " + quantity;
+        priceMessage += "\nTotal: $" + price;
+        priceMessage += "\nThank you!";
         return priceMessage;
     }
     /**
      * This method is called when the plus button is clicked.
      */
     public void increment(View view) {
+
+        if(quantity == 100) {
+            // Show an error message as a toast
+            Toast.makeText(this, "You cannot have more than 100 coffees", Toast.LENGTH_SHORT).show();
+            // Exit this method early because there's nothing left to do
+            return;
+        }
         quantity++;
         displayQuantity(quantity);
     }
@@ -77,7 +125,10 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the negative button is clicked.
      */
     public void decrement(View view) {
-        if(quantity == 0) {
+        if(quantity == 1) {
+            // Show an error message as a toast
+            Toast.makeText(this, "You cannot have less than 1 coffee", Toast.LENGTH_SHORT).show();
+            // Exit this method early because there's nothing left to do
             return;
         }
         quantity--;
@@ -93,8 +144,8 @@ public class MainActivity extends AppCompatActivity {
     /**
      * This method displays the given text on the screen.
      */
-    private void displayMessage(String message) {
+/*    private void displayMessage(String message) {
         TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
         orderSummaryTextView.setText(message);
-    }
+    }*/
 }
